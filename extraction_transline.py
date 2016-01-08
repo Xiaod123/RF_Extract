b29d28d35362c9b26d1f0123da2c15ca0d4b7197
wwahby <w.wahby@gmail.com>
2016-01-08 16:08:20 -0500

    First pass at lumped RLGC extraction method

--- extraction_transline.py
+++ extraction_transline.py
@@ -5,7 +5,7 @@ import skrf as rf # RF functions. To install this do "conda install -c scikit-rf
 import math
 
 
-def rlgc_from_s2p(length_m, s2p_filename, z0_probe=50):
+def distributed_rlgc_from_s2p(length_m, s2p_filename, z0_probe=50):
 	# length_m:	(m)	Length of structure being measured
 	# s2p_filename: (str)	s2p filename
 	# z0_probe:	(Ohms)	Impedance of network analyzer/probes. 50 Ohm default.
@@ -28,4 +28,26 @@ def rlgc_from_s2p(length_m, s2p_filename, z0_probe=50):
 
 	attenuation = 20*np.log10( abs(np.exp(-gamma*length_m)) )
 
-	return( net, freq, R, L, G, C, gamma, Zc)
+	return( freq, R, L, G, C, gamma, Zc, net )
+
+
+def lumped_rlgc_from_s2p(s2p_filename, z0_probe=50):
+	# s2p_filename: (str)	s2p filename
+	# z0_probe:	(Ohms)	Impedance of network analyzer/probes. 50 Ohm default.
+
+	net = rf.Network(s2p_filename, z0=z0_probe)
+
+	freq = net.f
+
+	Z = s.z
+	Y = s.y
+
+	Zdiff = [zz[0,0] - zz[0,1] - zz[1,0] + zz[1,1] for zz in Z]
+	Ycomm = [yy[0,0] + yy[0,1] + yy[1,0] + yy[1,1] for yy in Y]
+
+	R = real(Zdiff)
+	L = 1/2/pi/freq * imag(Zdiff)
+	G = real(Ycomm)
+	C = 1/2/pi/freq * imag(Ycomm)
+
+	return (freq, R, L, G, C, Zdiff, Ycomm, net)
