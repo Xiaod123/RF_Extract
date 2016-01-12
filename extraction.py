951f6715a458cc0960ea91986d0e71563529a646
wwahby <w.wahby@gmail.com>
2016-01-12 17:28:50 -0500

    key change -- added z0_probe in lumped rlgc extract -- it is now much closer to correct. Other changes - lots of tl rlgc functions tweaked

--- extraction.py
+++ extraction.py
@@ -17,16 +17,89 @@ def test():
 	
 	(freq, Sri_L, Sri_2L, abcd_L, abcd_2L, Sdb_L, Sdeg_L, Sdb_2L, Sdeg_2L, net_L, net_2L) = l2l_deembed(pad_L_s2p_filename, pad_2L_s2p_filename, structure_L_s2p_filename, structure_2L_s2p_filename)
 	(freq, R, L, G, C, gamma, attenuation, losstan, Zc) = distributed_rlgc_from_abcd(length_m, freq, abcd_L)
+	write_s_db_deg(Sdb_L, Sdeg_L, freq, "sdb_sdeg.csv")
 	write_rlgc(freq, R, L, G, C, "rlgc.csv")
 	write_rlgc(freq, gamma.real, gamma.imag, Zc.real, Zc.imag, "gamma.csv")
 	
+	(freq, R, L, G, C, gamma, attenuation, losstan, Zc) = distributed_rlgc_from_sdb(length_m, freq, Sdb_L, Sdeg_L)
+	write_rlgc(freq, R, L, G, C, "rlgc2.csv")
+	write_rlgc(freq, gamma.real, gamma.imag, Zc.real, Zc.imag, "gamma2.csv")
+	
+	(freq, R, L, G, C, gamma, attenuation, losstan, Zc) = distributed_rlgc_from_sri(length_m, freq, Sri_L)
+	write_rlgc(freq, R, L, G, C, "rlgc_sri.csv")
+	write_rlgc(freq, gamma.real, gamma.imag, Zc.real, Zc.imag, "gamma_sri.csv")
+	
 	(freq, R, L, G, C, Zdiff, Ycomm, net) = lumped_rlgc_from_Network(net_L)
 	write_rlgc(freq, R, L, G, C, "rlgc_lumped.csv")
+
+
 
 def distributed_rlgc_from_abcd(length_m, freq, abcd_mat_array, z0_probe=50):
 	# length_m:	(m)	Length of structure being measured
 	# s2p_filename: (str)	s2p filename
 	# z0_probe:	(Ohms)	Impedance of network analyzer/probes. 50 Ohm default.
+
+	gamma = np.zeros( (len(abcd_mat_array)), dtype=complex)
+	Zc = np.zeros( (len(abcd_mat_array)), dtype=complex)
+	d_vec = np.zeros( (len(abcd_mat_array)), dtype=complex)
+	b_vec = np.zeros( (len(abcd_mat_array)), dtype=complex)
+	
+	R = np.zeros( (len(abcd_mat_array)) )
+	L = np.zeros( (len(abcd_mat_array)) )
+	G = np.zeros( (len(abcd_mat_array)) )
+	C = np.zeros( (len(abcd_mat_array)) )
+	losstan = np.zeros( (len(abcd_mat_array)) )
+	attenuation = np.zeros( (len(abcd_mat_array)) )
+	
+	for idx in range(len(abcd_mat_array)):
+		abcd = abcd_mat_array[idx]
+		
+		d_vec[idx] = abcd[1][1]
+		b_vec[idx] = abcd[1][0]
+		
+		gamma[idx] = 1/length_m*np.arccosh(d_vec[idx])
+		Zc[idx] = b_vec[idx]**(-1) * np.sinh(gamma[idx] * length_m)
+		R[idx] = (gamma[idx] * Zc[idx]).real
+		L[idx] = 1/2/math.pi/freq[idx] * ( (gamma[idx] * Zc[idx]).imag)
+		G[idx] = (gamma[idx] / Zc[idx]).real
+		C[idx] = 1/2/math.pi/freq[idx] * ((gamma[idx]/Zc[idx]).imag)
+		
+		losstan[idx] = ( (gamma[idx]/Zc[idx]).real) / ( (gamma[idx]/Zc[idx]).imag)
+		attenuation[idx] = 20*np.log10( abs(np.exp( -gamma[idx] * length_m )) )
+		
+		#print(gamma[idx] * Zc[idx])
+		#print(1/2/math.pi/freq[idx] * ( (gamma[idx] * Zc[idx]).imag))
+
+
+#	R = ( gamma * Zc).real
+#	L = 1/2/math.pi/freq * ((gamma * Zc).imag )
+#	G = (gamma/Zc).real
+#	C = 1/2/math.pi/freq * (gamma/Zc).imag
+#	losstan = (gamma/Zc).real / (gamma/Zc).imag
+#
+#	attenuation = 20*np.log10( abs(np.exp(-gamma*length_m)) )
+
+	return ( freq, R, L, G, C, gamma, attenuation, losstan, Zc )
+	
+def distributed_rlgc_from_sdb(length_m, freq, Sdb, Sdeg, z0_probe=50):
+	# length_m:	(m)	Length of structure being measured
+	# s2p_filename: (str)	s2p filename
+	# z0_probe:	(Ohms)	Impedance of network analyzer/probes. 50 Ohm default.
+	
+	Sri_mat = np.zeros( (len(Sdb),2,2), dtype=complex)
+	
+	for idx in range(len(Sdb)):
+		Sdb_mat = Sdb[idx]
+		Sdeg_mat = Sdeg[idx]
+		
+		Sri_mat[idx][0][0] = 10**(Sdb_mat[0][0]/20) * ( np.cos(Sdeg_mat[0][0]*np.pi/180) + 1j*np.sin(Sdeg_mat[0][0]*np.pi/180) )
+		Sri_mat[idx][0][1] = 10**(Sdb_mat[0][1]/20) * ( np.cos(Sdeg_mat[0][1]*np.pi/180) + 1j*np.sin(Sdeg_mat[0][1]*np.pi/180) )
+		Sri_mat[idx][1][0] = 10**(Sdb_mat[1][0]/20) * ( np.cos(Sdeg_mat[1][0]*np.pi/180) + 1j*np.sin(Sdeg_mat[1][0]*np.pi/180) )
+		Sri_mat[idx][1][1] = 10**(Sdb_mat[1][1]/20) * ( np.cos(Sdeg_mat[1][1]*np.pi/180) + 1j*np.sin(Sdeg_mat[1][1]*np.pi/180) )
+		
+	abcd_mat_array = sri2abcd(Sri_mat)
+	
+	
 
 	gamma = np.zeros( (len(abcd_mat_array)), dtype=complex)
 	Zc = np.zeros( (len(abcd_mat_array)), dtype=complex)
@@ -68,6 +141,42 @@ def distributed_rlgc_from_abcd(length_m, freq, abcd_mat_array, z0_probe=50):
 	attenuation = 20*np.log10( abs(np.exp(-gamma*length_m)) )
 
 	return ( freq, R, L, G, C, gamma, attenuation, losstan, Zc )
+	
+def distributed_rlgc_from_sri(length_m, freq, Sri, z0_probe=50):
+	# length_m:	(m)	Length of structure being measured
+	# s2p_filename: (str)	s2p filename
+	# z0_probe:	(Ohms)	Impedance of network analyzer/probes. 50 Ohm default.
+
+	gamma = np.zeros( (len(Sri)), dtype=complex)
+	Zc =    np.zeros( (len(Sri)), dtype=complex)
+	d_vec = np.zeros( (len(Sri)), dtype=complex)
+	b_vec = np.zeros( (len(Sri)), dtype=complex)
+	
+	
+	for idx in range(len(Sri)):
+		
+		S11 = Sri[idx][0][0]
+		S12 = Sri[idx][0][1]
+		S21 = Sri[idx][1][0]
+		S22 = Sri[idx][1][1]
+		
+		K = np.sqrt( ((S11**2 - S21**2 + 1)**2 - (2*S11)**2)/(2*S21**2))
+		alpha = ( (1 - S11**2 + S21**2)/(2*S21) + K)**(-1)
+		gamma[idx] = -1/length_m * np.log(alpha)
+		
+		Zc[idx] = np.sqrt( z0_probe**2 * ( (1+S11)**2 - S21**2)/( (1-S11)**2 - S21**2) )
+		
+
+
+	R = ( gamma * Zc).real
+	L = 1/2/math.pi/freq * ((gamma * Zc).imag )
+	G = (gamma/Zc).real
+	C = 1/2/math.pi/freq * (gamma/Zc).imag
+	losstan = (gamma/Zc).real / (gamma/Zc).imag
+
+	attenuation = 20*np.log10( abs(np.exp(-gamma*length_m)) )
+
+	return ( freq, R, L, G, C, gamma, attenuation, losstan, Zc )
 
 
 def lumped_rlgc_from_Network(net, z0_probe=50):
@@ -85,7 +194,7 @@ def lumped_rlgc_from_Network(net, z0_probe=50):
 		zz = Z[idx]
 		yy = Y[idx]
 		
-		Zdiff[idx] = zz[0,0] - zz[0,1] - zz[1,0] + zz[1,1]
+		Zdiff[idx] = z0_probe*(zz[0,0] - zz[0,1] - zz[1,0] + zz[1,1])
 		Ycomm[idx] = yy[0,0] + yy[0,1] + yy[1,0] + yy[1,1]
 
 	R = Zdiff.real
@@ -156,26 +265,6 @@ def abcd2s_alt(abcd_struct, Z01, Z02):
 
 	return S
 	
-def abcd2s_alt2(abcd_struct, Z01, Z02):
-	# convert ABCD matrix to S matrix in real/imag format
-
-	#R01 = Z01.real
-	#R02 = Z02.real
-	#Z0 = Z01
-
-	num_freqs = len(abcd_struct)
-	S = np.zeros( (num_freqs, 2, 2), dtype=complex )
-	for idx in range(len(abcd_struct)):
-		mat = abcd_struct[idx]
-		S_mat = 1/mat
-
-		S[idx][0][0] = S_mat[0][0]
-		S[idx][0][1] = S_mat[0][1]
-		S[idx][1][0] = S_mat[1][0]
-		S[idx][1][1] = S_mat[1][1]
-
-
-	return S
 
 def sri2abcd(s_struct, Z01=50, Z02=50):
 	# Convert Sparams in Real/Imag format to ABCD matrix
@@ -197,6 +286,33 @@ def sri2abcd(s_struct, Z01=50, Z02=50):
 		B = ( (np.conj(Z01) + S11*Z01)*(np.conj(Z02)+S22*Z02)-S12*S21*Z01*Z02 ) / denom
 		C = ( (1-S11)*(1-S22)-S12*S21 ) / denom
 		D = ( (1-S11)*(np.conj(Z02)+S22*Z02) + S12*S21*Z02 ) / denom
+
+		abcd[idx][0][0] = A
+		abcd[idx][0][1] = B
+		abcd[idx][1][0] = C
+		abcd[idx][1][1] = D
+
+	return abcd
+	
+def sri2abcd_alt(s_struct, Z01=50, Z02=50):
+	# Convert Sparams in Real/Imag format to ABCD matrix
+	R01 = Z01.real
+	R02 = Z02.real
+	Z0 = Z01
+	num_freqs = len(s_struct)
+	abcd = np.zeros( (num_freqs, 2, 2), dtype=complex )
+	for idx in range(len(s_struct)):
+		mat = s_struct[idx]
+		S11 = mat[0][0]
+		S12 = mat[0][1]
+		S21 = mat[1][0]
+		S22 = mat[1][1]
+		
+		
+		A = ( (1+S11)*(1-S22) + S12*S21 ) / (2*S21)
+		B = Z0*( ((1+S11)*(1+S22) - S12*S21) / (2*S21) )
+		C = ( (1-S11)*(1-S22) - S12*S21) / (2*S21*Z0) 
+		D = ( (1-S11)*(1+S22) + S12*S21) / (2*S21)
 
 		abcd[idx][0][0] = A
 		abcd[idx][0][1] = B
@@ -271,14 +387,14 @@ def sri2sdb(sri_struct):
 		S21_deg = np.arcsin( S21_ri.imag / np.abs(S21_ri) ) * 180/math.pi
 		S22_deg = np.arcsin( S22_ri.imag / np.abs(S22_ri) ) * 180/math.pi
 		
-		#if ( S11_ri.real < 0 ) and (S11_ri.imag > 0):
-		#	S11_deg = 180 - S11_deg
-		#if ( S12_ri.real < 0 ) and (S12_ri.imag > 0):
-		#	S12_deg = 180 - S12_deg
-		#if ( S21_ri.real < 0 ) and (S21_ri.imag > 0):
-		#	S21_deg = 180 - S21_deg
-		#if ( S22_ri.real < 0 ) and (S22_ri.imag > 0):
-		#	S22_deg = 180 - S22_deg
+#		if ( S11_ri.real < 0 ) and (S11_ri.imag > 0):
+#			S11_deg = 180 - S11_deg
+#		if ( S12_ri.real < 0 ) and (S12_ri.imag > 0):
+#			S12_deg = 180 - S12_deg
+#		if ( S21_ri.real < 0 ) and (S21_ri.imag > 0):
+#			S21_deg = 180 - S21_deg
+#		if ( S22_ri.real < 0 ) and (S22_ri.imag > 0):
+#			S22_deg = 180 - S22_deg
 		
 
 		Sdb[idx][0][0] = S11_db
@@ -391,7 +507,7 @@ def write_s_db_deg( sdb, sdeg, freq, filename):
 		S21_deg = s_deg_mat[1][0]
 		S22_deg = s_deg_mat[1][1]
 
-		outstr = "{0:.4g},{1:f},{2:f},{3:f},{4:f},{5:f},{6:f},{7:f},{8:f}\n".format( freq, S11_db, S11_deg, S12_db, S12_deg, S21_db, S21_deg, S22_db, S22_deg)
+		outstr = "{0:.8g},{1:.8g},{2:.8g},{3:.8g},{4:.8g},{5:.8g},{6:.8g},{7:.8g},{8:.8g}\n".format( freq, S11_db, S11_deg, S12_db, S12_deg, S21_db, S21_deg, S22_db, S22_deg)
 
 		outfile.write(outstr)
 
