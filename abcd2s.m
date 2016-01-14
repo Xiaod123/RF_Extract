5ca55d402033d794c8d5a8918376486c54f290a3
wwahby <w.wahby@gmail.com>
2016-01-14 18:42:32 -0500

    Swapped back to original matlab abcd2s since it's the same

--- abcd2s.m
+++ abcd2s.m
@@ -18,3 +18,17 @@ S21 = ( 2*sqrt(R01*R02) ) / denom;
 S22 = (-A*conj(Z02) + B - C*Z01*conj(Z02) + D*Z01 ) / denom;
 
 S = [ S11 S12 ; S21 S22 ];
+
+
+% function [s_new] = abcd2s(abcd,z0)
+% 
+% A = abcd(1,1);
+% B = abcd(1,2);
+% C = abcd(2,1);
+% D = abcd(2,2);
+% divider = A+(B/z0)+(C*z0)+D;
+% s11new = (A+(B/z0)-(C*z0)-D)/divider;
+% s12new = (2*(A*D-B*C))/divider;
+% s21new = 2/divider;
+% s22new = (-A+(B/z0)-(C*z0)+D)/divider;
+% s_new = [s11new s12new; s21new s22new];
